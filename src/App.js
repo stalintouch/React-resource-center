dc99dca215bbfd44f1523c18032eabe805f0f9e7
stalintouch <stalintouch1983@gmail.com>
2017-10-05 18:22:48 -0400

    removed some dependencies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar'
-import logo from './logo.svg';
-import './App.css';
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+import './App.css'
+import { BrowserRouter, Link } from 'react-router-dom'
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = { open: false }
+  }
+
+  handleClose = () => {
+    this.setState({ open: false })
+  }
+
+  render () {
     return (
-      <div className="App">
-        <AppBar
-          title="title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-      </div>
-    );
+      <BrowserRouter>
+        <div className='App'>
+          <AppBar
+            title='title'
+            iconClassNameRight='muidocs-icon-navigation-expand-more'
+            onLeftIconButtonTouchTap={() =>
+              this.setState({ open: !this.state.open })}
+          />
+          <Drawer
+            open={this.state.open}
+            docked={false}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <Link to='/'>
+              <MenuItem onClick={this.handleClose}>Home</MenuItem>
+            </Link>
+          </Drawer>
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MuiThemeProvider } from 'material-ui';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<MuiThemeProvider> <App /></MuiThemeProvider>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
